4f44533744edd6fd83084a457c94d295d3134b17
Robiiiiiiiiiiiin <robinbecherraz2@hotmail.fr>
2020-11-18 18:31:42 +0100

    integration

--- src/js/index.js
+++ src/js/index.js
@@ -1,81 +1,83 @@
 var currentSlide = 0;
+var selector = "section";
+var $slides = $(selector);
+var isAnimating = false;
 
 // ==================================smooth-scroll-start==================================
- 
-(function($) {
-    var selector = "section";
-  
-    var $slides = $(selector);
-  
-    var isAnimating = false;
-  
-    var stopAnimation = function() {
-      console.log(currentSlide)
-      setTimeout(function() {
-        isAnimating = false;
-      }, 300);
-    };
-  
-    var bottomIsReached = function($elem) {
-      var rect = $elem[0].getBoundingClientRect();
-      return rect.bottom <= $(window).height();
-    };
-  
-    var topIsReached = function($elem) {
-      var rect = $elem[0].getBoundingClientRect();
-      return rect.top >= 0;
-    };
-  
-    document.addEventListener(
-      "wheel",
-      function(event) {
-        var $currentSlide = $($slides[currentSlide]);
-  
-        if (isAnimating) {
-          event.preventDefault();
-          return;
-        }
-  
-        var direction = -event.deltaY;
-  
-        if (direction < 0) {
-          // next
-          if (currentSlide + 1 >= $slides.length) return;
-          if (!bottomIsReached($currentSlide)) return;
-          event.preventDefault();
-          currentSlide++;
-          var $slide = $($slides[currentSlide]);
-          var offsetTop = $slide.offset().top;
-          isAnimating = true;
-          $("html, body").animate(
-            {
-              scrollTop: offsetTop
-            },
-            1000,
-            stopAnimation
-          );
-        } else {
-          // back
-          if (currentSlide - 1 < 0) return;
-          if (!topIsReached($currentSlide)) return;
-          event.preventDefault();
-          currentSlide--;
-          var $slide = $($slides[currentSlide]);
-          var offsetTop = $slide.offset().top;
-          isAnimating = true;
-          $("html, body").animate(
-            {
-              scrollTop: offsetTop
-            },
-            1000,
-            stopAnimation
-          );
-        }
+
+var stopAnimation = function () {
+  console.log(currentSlide)
+  setTimeout(function () {
+    isAnimating = false;
+  }, 300);
+};
+
+var bottomIsReached = function ($elem) {
+  var rect = $elem[0].getBoundingClientRect();
+  return rect.bottom <= $(window).height();
+};
+
+var topIsReached = function ($elem) {
+  var rect = $elem[0].getBoundingClientRect();
+  return rect.top >= 0;
+};
+
+function handleSlide() {
+  var $currentSlide = $($slides[currentSlide]);
+
+  if (isAnimating) {
+    event.preventDefault();
+    return;
+  }
+
+  var direction = -event.deltaY;
+
+  if (direction < 0) {
+    // next
+    if (currentSlide + 1 >= $slides.length) return;
+    if (!bottomIsReached($currentSlide)) return;
+    event.preventDefault();
+    currentSlide++;
+    var $slide = $($slides[currentSlide]);
+    var offsetTop = $slide.offset().top;
+    isAnimating = true;
+    $("html, body").animate(
+      {
+        scrollTop: offsetTop
       },
-      { passive: false }
+      1000,
+      stopAnimation
     );
-  })(jQuery);
-  // ==================================smooth-scroll-end==================================
+  } else {
+    // back
+    if (currentSlide - 1 < 0) return;
+    if (!topIsReached($currentSlide)) return;
+    event.preventDefault();
+    currentSlide--;
+    var $slide = $($slides[currentSlide]);
+    var offsetTop = $slide.offset().top;
+    isAnimating = true;
+    $("html, body").animate(
+      {
+        scrollTop: offsetTop
+      },
+      1000,
+      stopAnimation
+    );
+  }
+}
+
+document.addEventListener("wheel", function (event) {
+    handleSlide(event)
+  },
+  { passive: false }
+);
+
+$('iframe').contents().on('scroll', (evt) => {
+  console.log("scroll iframe")
+  handleSlide(evt)
+})
+// ==================================smooth-scroll-end==================================
 
 
 //nav-start
@@ -87,7 +89,7 @@ var btns = btnContainer.getElementsByClassName("btn");
 
 // Loop through the buttons and add the active class to the current/clicked button
 for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
+  btns[i].addEventListener("click", function () {
     var current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
